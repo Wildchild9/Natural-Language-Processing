96d9681425890220da8d0c893a5e7beae247b21f
Noah <noahleviwilder@gmail.com>
2019-03-14 19:15:16 -0400

    Initial Commit

---
+++ Natural Language Procession/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Natural Language Procession
+//
+//  Created by Noah Wilder on 2019-03-14.
+//  Copyright © 2019 Noah Wilder. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
